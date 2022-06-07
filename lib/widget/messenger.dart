eefb8c5588df5c12666767967996e273900e6e16
Matheus <math.moura@hotmail.com>
2022-06-07 19:56:21 +0200

    textfiled and send button layout

--- lib/widget/messenger.dart
+++ lib/widget/messenger.dart
@@ -23,11 +23,42 @@ class Messenger extends StatelessWidget {
             decoration: const BoxDecoration(
               color: Colors.amber,
             ),
-            child: Row(
-              children: const [
-                Text("Hello"),
-                Text("Hello"),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Enter your username',
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    DecoratedBox(
+                      decoration: const BoxDecoration(
+                        color: Colors.transparent,
+                      ),
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        icon: const Icon(Icons.arrow_forward),
+                        color: Colors.black,
+                        iconSize: 40.0,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         )
